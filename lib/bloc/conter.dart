7a7809efddedbd7fd630ec7ebbd3a91c077042ed
white.wolf <white.wolf@gmail.com>
2025-08-07 01:11:15 +0700

    Bloc Builder Done

---
+++ lib/bloc/conter.dart
@@ -0,0 +1,21 @@
+import 'package:bloc/bloc.dart';
+
+class CounterCubit extends Cubit<int> {
+  CounterCubit({this.init = 0}) : super(init);
+
+  int init;
+  void increment() => emit(state + 1);
+  void decrement() => emit(state - 1);
+
+  @override
+  void onChange(Change<int> change) {
+    super.onChange(change);
+    print('CounterCubit state changed: $change');
+  }
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    super.onError(error, stackTrace);
+    print('CounterCubit error: $error');
+  }
+}
--- lib/bloc_builder_page.dart
+++ lib/bloc_builder_page.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_app/bloc/conter.dart';
 
 class BlocBuilderPage extends StatelessWidget {
-  const BlocBuilderPage({super.key});
+  BlocBuilderPage({super.key});
+
+  CounterCubit _counterCubit = CounterCubit();
 
   @override
   Widget build(BuildContext context) {
@@ -14,11 +18,50 @@ class BlocBuilderPage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.black,
       ),
-      body: Center(
-        child: Text(
-          'This is the Bloc Builder Page',
-          style: TextStyle(fontSize: 24, color: Colors.deepPurple),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // StreamBuilder(
+          //   stream: _counterCubit.stream,
+          //   builder: (context, asyncSnapshot) {
+          //     return Text(
+          //       '${asyncSnapshot.data ?? 0}',
+          //       style: TextStyle(fontSize: 50),
+          //     );
+          //   },
+          // ),
+          BlocBuilder<CounterCubit, int>(
+            bloc: _counterCubit,
+            buildWhen: (previous, current) {
+              if (current % 2 == 0) {
+                print('Counter is a multiple of 5: $current');
+                return true;
+              } else {
+                print('Counter is not a multiple of 5: $current');
+                return false;
+              }
+            },
+            builder: (context, state) =>
+                Text('$state', style: TextStyle(fontSize: 50)),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                onPressed: () {
+                  _counterCubit.decrement();
+                },
+                icon: const Icon(Icons.remove, size: 50, color: Colors.red),
+              ),
+              IconButton(
+                onPressed: () {
+                  _counterCubit.increment();
+                },
+                icon: const Icon(Icons.add, size: 50, color: Colors.green),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
